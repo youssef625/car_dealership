e9472ea94e02dcccd319f893d21b778e55c80843
youssef625 <yaussefahmed@gmail.com>
2025-11-18 14:38:19 +0200

    initate

---
+++ src/main/java/com/swe/car_dealership/CarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.swe.car_dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipApplication.class, args);
+	}
+
+}
