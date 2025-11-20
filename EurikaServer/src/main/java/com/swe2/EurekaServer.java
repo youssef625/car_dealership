a325e0ad6dbd1c8b0fedc349813c4dd720faffaa
youssef625 <yaussefahmed@gmail.com>
2025-11-20 11:10:19 +0200

    setting up eurika server

---
+++ EurikaServer/src/main/java/com/swe2/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.swe2;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    static void main(String[] args) {
+        SpringApplication.run(
+                EurekaServer.class,
+                args
+        );
+    }
+}
---
+++ UserManagement/src/main/java/com/swe2/UserManagement.java
@@ -0,0 +1,13 @@
+package com.swe2;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UserManagement {
+    static void main(String[] args) {
+        SpringApplication.run(UserManagement.class,args);
+    }
+}
