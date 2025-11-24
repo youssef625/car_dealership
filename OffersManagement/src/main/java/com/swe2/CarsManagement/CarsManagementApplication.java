e8e1203a92d3fe4b09c3e06d1699c24791bee94d
youssef625 <yaussefahmed@gmail.com>
2025-11-24 18:17:41 +0200

    adding middleware

---
+++ OffersManagement/src/main/java/com/swe2/CarsManagement/CarsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.swe2.CarsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CarsManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarsManagementApplication.class,args);
+    }
+}
